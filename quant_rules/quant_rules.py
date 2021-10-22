01b909756f9e7e65ccd2a21087c8d63f73783887
colivarese <ces.oe97@gmail.com>
2021-10-21 23:44:01 -0500

    changes

--- quant_rules/quant_rules.py
+++ quant_rules/quant_rules.py
@@ -1,2 +1,3 @@
 import numpy as np
+from utils import *
 
--- quant_rules/utils.py
+++ quant_rules/utils.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import numpy as np
 
 def readFile(file):
     return pd.read_csv(file, index=False)
-    print('hi')
+
+def sortOnIdndex(array):
+    pass
+
+def Window(array,dim, minDif):
+    avg = np.mean(array[:,dim]) + minDif
+    sorted(array, key=lambda a: a[dim])
+    return sorted
