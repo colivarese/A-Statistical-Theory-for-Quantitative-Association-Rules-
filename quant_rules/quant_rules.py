2c4052ecfe78823544016f12811526def0b2aa5a
colivarese <ces.oe97@gmail.com>
2021-10-22 09:57:53 -0500

    Update

--- quant_rules/quant_rules.py
+++ quant_rules/quant_rules.py
@@ -1,3 +1,7 @@
 import numpy as np
 from utils import *
 
+dataset = readFile('./dataset/Hours_age.csv')
+
+dataset = Window(dataset, 0, 1)
+print(dataset)
--- quant_rules/utils.py
+++ quant_rules/utils.py
@@ -2,12 +2,16 @@ import pandas as pd
 import numpy as np
 
 def readFile(file):
-    return pd.read_csv(file, index=False)
+    df = pd.read_csv(file)
+    return df.to_numpy()
 
-def sortOnIdndex(array):
-    pass
+def sortOnIndex(array, dim):
+    return sorted(array, key=lambda a: a[dim])
+
+def getDimension(array, dim):
+    return [a[dim] for a in array]
 
 def Window(array,dim, minDif):
-    avg = np.mean(array[:,dim]) + minDif
-    sorted(array, key=lambda a: a[dim])
-    return sorted
+    avg = np.mean(getDimension(array,dim)) + minDif
+    sorted_arr = sortOnIndex(array,dim)
+    return sorted_arr
