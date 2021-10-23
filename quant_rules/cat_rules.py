b8cbcd4dff5b4006ea69e0c2973a9ddb7cf8ee4d
colivarese <ces.oe97@gmail.com>
2021-10-22 21:51:59 -0500

    CATEGORICAL PRINTING

--- quant_rules/cat_rules.py
+++ quant_rules/cat_rules.py
@@ -4,17 +4,23 @@ from apriori import *
 from utilsApriori import *
 dataset = readFile('./dataset/categorical.csv', asArray=False)
 
-cat_dataset = extractCategorical(dataset,asArray=True)
-reversed_cat = list([list(reversed(i)) for i in cat_dataset])
 
+cat_dataset = extractCategorical(dataset,asArray=True)
+reversed_cat = [set(i) for i in cat_dataset]
+
+dataset = dataset.to_numpy()
 freq_itemset = aprioriFromFile('./dataset/categorical.csv',400,100)
 n = len(freq_itemset[0])
 keys = range(0,n)
 freqs = {key: [] for key in keys}
 for i in range(n):
     for its in freq_itemset[0][i]:
-        freqs[i].append(list(its))
+        freqs[i].append(set(its))
 
-print(freqs[2][0] in reversed_cat)
-print(freqs[2][0])
-print(reversed_cat[10])
+
+for freq in freqs[2]:
+    indices = [i for i, x in enumerate(reversed_cat) if x == freq]
+    tmp_sum = []
+    for idx in indices:
+        tmp_sum.append(dataset[idx][3])
+    print(freq, "==>", np.mean(tmp_sum))
