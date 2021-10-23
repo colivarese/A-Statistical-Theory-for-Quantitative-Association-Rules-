13318e9564b6ec22210249c312989796189118e7
colivarese <ces.oe97@gmail.com>
2021-10-23 12:45:21 -0500

    update

--- quant_rules/cat_rules.py
+++ quant_rules/cat_rules.py
@@ -3,7 +3,7 @@ from utils import *
 from apriori import *
 from utilsApriori import *
 import os
-dataset = readFile('./dataset/categorical.csv', asArray=False)
+dataset = readFile('./dataset/categorical_two.csv', asArray=False)
 
 
 cat_dataset = extractCategorical(dataset,asArray=True)
@@ -12,7 +12,7 @@ pd.DataFrame(reversed_cat).to_csv('./dataset/categorical_only.csv')
 quant_dataset = extractNumerical(dataset,asArray=True)
 
 #freq_itemset = aprioriFromFile('./dataset/categorical.csv',400,100)
-freq_itemset = aprioriFromFile('./dataset/categorical_only.csv',700,100)
+freq_itemset = aprioriFromFile('./dataset/categorical_only.csv',400,100)
 n = len(freq_itemset[0])
 keys = range(0,n)
 freqs = {key: [] for key in keys}
@@ -20,7 +20,7 @@ for i in range(n):
     for its in freq_itemset[0][i]:
         freqs[i].append(set(its))
 
-for freq in freqs[2]:
+for freq in freqs[len(freqs)-1]:
     indices = [i for i, x in enumerate(reversed_cat) if x == freq]
     tmp_sum = []
     for idx in indices:
